69b1aafacbae3099ca684fb245c8f4fd0d0b2020
ebomberger23 <ebomberger@gmail.com>
2018-07-13 17:40:53 -0400

    typo
    typo

--- util.py
+++ util.py
@@ -2,5 +2,5 @@ def textToNum(text): # pep8 says this should be text_to_bin, but you're using ca
   res = ""
   for char in text:
     assert(ord(char) < 256) # divToAscii only supports single-byte chars
-    res += f"f"{ord(char):08b}" # binary ord padded to 8 bits
+    res += f"{ord(char):08b}" # binary ord padded to 8 bits
   return int(res, 2)
