39c8c8c12254a556a9ef1e087b5344720a4aac17
HappyFakeBoulder <40407420+HappyFakeBoulder@users.noreply.github.com>
2018-07-14 22:57:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # IMPORTANT NOTE:
-# This program reads code from code.txt.
+# This program reads code from code.stkl.
 # Before running, edit code.txt to hold the
 # program you want to run.
 
-import pdb
+import time
 # allows code errors
 class CodeError(Exception):
 	def __init__(self, *args, **kwargs):
 		Exception.__init__(self, *args, **kwargs)
 # some helpful little functions
+def extendz(string):
+	while len(string) < 8:
+		string = "0" + string
+	return string
 def divideStr(string, sectLen):
 	return [string[start:start+sectLen] for start in range(0, len(string), sectLen)]
 def divideToChars(string):
@@ -21,6 +25,11 @@ def divToAscii(string):
 			raise ValueError("Non-binary input to divToAscii")
 	for x in divideToChars(string):
 		retVal += chr(int(x, 2))
+	return retVal
+def strToBin(string):
+	retVal = ""
+	for x in string:
+		retVal += extendz(bin(ord(x))[2:])
 	return retVal
 # the main interpreter class
 class StackLangInterpreter():
@@ -55,39 +64,51 @@ class StackLangInterpreter():
 				self.genPurpVar = x[x.find(y):z + 6]
 				self.dataStack.append(bin(int(self.genPurpVar))[2:])
 			# pop instruction
-			if x.startswith("pop"):
+			elif x.startswith("pop"):
 				try:
-					del self.dataStack[len(self.dataStack) - 1]
+					del self.dataStack[-1]
 				except IndexError:
 					raise CodeError("Line " + str(self.parsedCode.index(x) + 1) + ": Attempt to pop with an empty stack")
 			# concat instruction
-			if x.startswith("concat"):
+			elif x.startswith("concat"):
 				# check stack size
 				if len(self.dataStack) < 2:
 					raise CodeError("Line " + str(self.parsedCode.index(x) + 1) + ": Not enough items on stack to concatenate")
 				# get concatenation
-				self.genPurpVar = self.dataStack[len(self.dataStack) - 2]
-				self.genPurpVar += self.dataStack[len(self.dataStack) - 1]
+				self.genPurpVar = self.dataStack[-2]
+				self.genPurpVar += self.dataStack[-1]
 				# pop 2 times
-				del self.dataStack[len(self.dataStack) - 1]
-				del self.dataStack[len(self.dataStack) - 1]
+				del self.dataStack[-1]
+				del self.dataStack[-1]
 				# leave result on stack
 				self.dataStack.append(self.genPurpVar)
 			# copy instruction
-			if x.startswith("copy"):
-				self.dataStack.append(self.dataStack[len(self.dataStack) - 1])
+			elif x.startswith("copy"):
+				self.dataStack.append(self.dataStack[-1])
 			# swap instruction
-			if x.startswith("swap"):
-				self.genPurpVar = self.dataStack[len(self.dataStack) - 1]
-				del self.dataStack[len(self.dataStack) - 1]
+			elif x.startswith("swap"):
+				self.genPurpVar = self.dataStack[-1]
+				del self.dataStack[-1]
 				self.dataStack.insert(len(self.dataStack) - 1, self.genPurpVar)
 			# exec instruction
-			if x.startswith("exec"):
-				self.interpret(divToAscii(self.dataStack[len(self.dataStack) - 1]))
+			elif x.startswith("exec"):
+				self.interpret(divToAscii(self.dataStack[-1]))
+			# input instruction
+			elif x.startswith("input"):
+				self.dataStack.append(strToBin(input("> ")))
+			# output instruction
+			elif x.startswith("output"):
+				print("> " + divToAscii(self.dataStack[-1]))
+			# flip instruction
+			elif x.startswith("flip"):
+				self.dataStack = self.dataStack[::-1]
+			# all others
+			elif (not x.startswith("//")) and len(x.strip()) != 0:
+				raise CodeError("Command not found.")
 # programming interface
 interpreter = StackLangInterpreter()
-codeFile = open("code.txt", "r")
+codeFile = open("code.stkl", "r")
 userCode = codeFile.read()
 codeFile.close()
 interpreter.interpret(userCode)
-print(interpreter.dataStack)
+time.sleep(1)
